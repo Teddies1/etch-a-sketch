bc1f7e64cded0bcf29531bdb80f4dc5a3576c2d3
Teddies1 <nghonjoo@gmail.com>
2024-04-28 18:28:58 +0800

    finished asking user input for grid size

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const createDivs = (n) => {
     var container = document.querySelector(".div-container");
     for (let i = 0; i < n; i++){
         var row = document.createElement("div");
-        row.style.display = 'flex';
         row.style.flex = 1;
         row.classList.add("row");
 
@@ -10,10 +9,10 @@ const createDivs = (n) => {
             var squareDiv = document.createElement("div");
 
             squareDiv.classList.add("box");
-            squareDiv.style.width = "50px";
-            squareDiv.style.height = "50px";
+            squareDiv.style.width = "20px";
+            squareDiv.style.height = "20px";
             squareDiv.style.border = "1px solid black";
-            squareDiv.style.margin = "1px";
+            squareDiv.style.gap = "1px";
 
             row.appendChild(squareDiv);
         }
@@ -26,13 +25,37 @@ const applyHover = () => {
     boxes.forEach((box) => {
         box.addEventListener("mouseenter", (e) => {
             e.target.style.backgroundColor = "blue";
-        })
+        });
         // box.addEventListener("mouseleave", (e) =>{
         //     e.target.style.backgroundColor = "white";
         // })
-    })
+        
+    });
+}
+
+const promptValue = () => {
+    while (true){
+        var number = prompt("Enter a grid size less than 50: ");
+        if (number <= 50){
+            return number;
+        }   
+    }
 }
 
 
-createDivs(16)
-applyHover();
+const etchSketch = () => {
+    var gridSize = promptValue();
+    createDivs(gridSize);
+    applyHover();
+}
+
+var button = document.querySelector("button");
+button.addEventListener("click", () => {
+    var container = document.querySelector(".div-container");
+    container.innerHTML = '';
+    console.log(container);
+    etchSketch();
+});
+
+
+etchSketch();
