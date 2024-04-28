850b3b985a7fa38458e9ab075128c9943222e93d
Teddies1 <nghonjoo@gmail.com>
2024-04-28 16:11:42 +0800

    finished create grid logic

--- script.js
+++ script.js
@@ -2,21 +2,18 @@ function createDivs(n){
     var container = document.querySelector(".div-container");
     for (let i = 0; i < n; i++){
         var row = document.createElement("div");
-        row.classList.add("row-" + (i+1));
+        row.style.display = 'flex';
+        row.style.flex = 1;
+        row.classList.add("row");
+
         for (let j = 0; j < n; j++){
             var squareDiv = document.createElement("div");
 
-            squareDiv.style.width = "25px";
-            squareDiv.style.height = "25px";
-            squareDiv.style.border = "4px solid red";
-            squareDiv.style.display = 'inline-block';
-            squareDiv.style.margin = '1px';
-
-
-
-
-
-
+            squareDiv.classList.add("box");
+            squareDiv.style.width = "50px";
+            squareDiv.style.height = "50px";
+            squareDiv.style.border = "1px solid black";
+            squareDiv.style.margin = "1px";
 
             row.appendChild(squareDiv);
         }
